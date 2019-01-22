3053128cfd591c1c2a25365c31a7076890c56515
ajb85 <ajbrush85@gmail.com>
2019-01-22 17:51:52 -0500

    Finished array-methods & callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,11 +1,22 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a callback function and invoke the function to test your work.
+// You have been provided an example of a problem and a solution to see how this
+// works with our items array.  Study both the problem and the solution to figure
+// out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
+function callback(param) {
+  console.log(param);
+}
+getLength(items, callback); // 4
+last(items, callback); // Gum
+sumNums(4, 5, callback); // 9
+multiplyNums(4, 5, callback); // 20
+contains("Pencil", items, callback); // true
+contains("Pen", items, callback); // false
+/*
 
-/* 
+  //Given this problem:
 
-  //Given this problem: 
-  
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -21,26 +32,30 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.indexOf(item) > -1);
 }
 
 /* STRETCH PROBLEM */
